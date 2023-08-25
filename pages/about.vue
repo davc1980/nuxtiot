950eafc19c6f40998393c54b34df4e856e4a4a93
davc1980 <davc1980@gmail.com>
2023-08-25 08:34:59 -0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+        <a href="/">home</a>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>hi dav</h1>
+    <a href="/about">About</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
